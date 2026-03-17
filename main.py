c7524b77c1932ce6ae9a09d2fcd77db933904adf
Серёжа <150809885+szarkans@users.noreply.github.com>
2026-03-17 15:27:00 +0300

    init

---
+++ main.py
@@ -0,0 +1,34 @@
+from bot.bot import bot
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+token = os.getenv("DISCORD_TOKEN")
+
+def load_extension():
+    extensions = [
+        ### Slash commands
+        "slash_commands.admin",
+
+        # Prefix commands
+        "commands.faq",
+        "commands.general",
+
+        ### Handlers
+        "handlers.bugs",
+        "handlers.role_select",
+        "handlers.punishments",
+        "handlers.get_help.admin_ticket",
+        "handlers.get_help.engine",
+
+        ### Other
+        "utils",
+        "storage",
+        "misc",
+        "flag_system.flag_commands",
+    ]
+    for i in extensions:
+        bot.load_extension(f"bot.{i}")
+
+load_extension()
+bot.run(token)
