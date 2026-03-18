f921f601e625d7d537439ca36af3a951c56fdc81
Серёжа <150809885+szarkans@users.noreply.github.com>
2026-03-18 11:23:52 +0300

    fix: я же сказал падай!!!!

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def load_extension():
         "handlers.role_select",
         "handlers.punishments",
         "handlers.get_help.admin_ticket",
-        "handlers.get_help.engine",
+        "handlers.get_help.engine"
 
         ### Other
         "utils",
