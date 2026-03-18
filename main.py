4995623665b70b821ff7d791bcc4a855757e8cc7
Серёжа <150809885+szarkans@users.noreply.github.com>
2026-03-18 11:47:45 +0300

    ещё раз падай, падаль

--- main.py
+++ main.py
@@ -18,8 +18,8 @@ def load_extension():
         "handlers.bugs",
         "handlers.role_select",
         "handlers.punishments",
-        "handlers.get_help.admin_ticket",
-        "handlers.get_help.engine",
+        "handlers.get_help.admin_ticket"
+        "handlers.get_help.engine"
 
         ### Other
         "utils",
